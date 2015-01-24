ef628102081a8bfb6fd7ff560eccb70800342ad9
Steven Roper <steven.m.roper@gmail.com>
2015-01-24 11:34:47 -0700

    finished 1st problem.

--- practice.js
+++ practice.js
@@ -3,9 +3,11 @@
 
 //Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
 
-  //Code here
+var me = {};
+me.name = "Steven";
+me.age = 26;
 
-
+alert(me.name);
 
 
 //NEXT PROBLEM
